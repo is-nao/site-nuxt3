2b4ffbda4a959c6260f25e36e0be3765446f3199
c-nao27 <78206853+c-nao27@users.noreply.github.com>
2025-03-22 16:02:01 +0000

    Deploying to gh-pages from @ c-nao27/site-nuxt3@52efd789cf0c125b7ac6f18838a7dec73b69f0bc 🚀

--- _nuxt/D549Nlb0.js
+++
@@ -1,1 +0,0 @@
-import{d as o,c as r,i as a,P as s,an as l,j as n}from"./C_TH93aK.js";const c=o({__name:"Alert",props:{color:{type:String,default:"orange"}},setup(e){return(t,d)=>(a(),r("div",{class:"alert",style:l({"border-color":e.color})},[s(t.$slots,"default",{},void 0,!0)],4))}}),p=Object.assign(n(c,[["__scopeId","data-v-46dbbe98"]]),{__name:"Alert"});export{p as default};
